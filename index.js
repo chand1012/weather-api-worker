14658345313f4d70147368a17dad91f1cae45397
Chandler Lofland <chandlerl14@outlook.com>
2020-05-09 21:07:17 -0400

    formated javascript

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const fetch = require('node-fetch')
-const DEFAULT_HEADERS = {'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive', 'User-Agent': 'node-fetch'};
-
+const DEFAULT_HEADERS = {
+  'Accept-Encoding': 'gzip, deflate',
+  Accept: '*/*',
+  Connection: 'keep-alive',
+  'User-Agent': 'node-fetch',
+}
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
@@ -30,13 +34,18 @@ async function handleRequest(request) {
 }
 
 async function getWeatherData(data) {
-  const getForecastURL = 'https://api.weather.gov/points/' + data.lat+ ',' + data.lng;
-  // console.log(getForecastURL);
-  let latlngResp = await fetch(getForecastURL, {method: 'GET', headers: DEFAULT_HEADERS});
-  const forecastNode = await latlngResp.json();
-  const forecastURL = forecastNode.properties.forecast;
-  // console.log(forecastURL);
-  let forecastResp = await fetch(forecastURL, {method: 'GET', headers: DEFAULT_HEADERS});
-  let returnForecast = await forecastResp.json();
-  return returnForecast;
+  const getForecastURL =
+    'https://api.weather.gov/points/' + data.lat + ',' + data.lng
+  let latlngResp = await fetch(getForecastURL, {
+    method: 'GET',
+    headers: DEFAULT_HEADERS,
+  })
+  const forecastNode = await latlngResp.json()
+  const forecastURL = forecastNode.properties.forecast
+  let forecastResp = await fetch(forecastURL, {
+    method: 'GET',
+    headers: DEFAULT_HEADERS,
+  })
+  let returnForecast = await forecastResp.json()
+  return returnForecast
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "target": "webworker",
-    "entry": "./index.js",
-    "mode": "production"
-  }
+  target: 'webworker',
+  entry: './index.js',
+  mode: 'production',
+}
